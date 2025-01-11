c4fcceb995c203eed50b2801fa5d70b4ede532e5
waynetheisinger <wayne@mowdirect.co.uk>
2025-01-11 18:11:22 +0000

    feature: add fortify

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return response()->json(['message' => 'Hello World!'],200);
+});
+
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
