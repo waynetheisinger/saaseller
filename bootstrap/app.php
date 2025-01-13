343b9931df8ddcc1b27ecc6f16e360098d0962b0
waynetheisinger <wayne@mowdirect.co.uk>
2025-01-13 06:42:39 +0000

    add sanctum middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -20,7 +20,7 @@ return Application::configure(basePath: dirname(__DIR__))
             'verified' => \App\Http\Middleware\EnsureEmailIsVerified::class,
         ]);
 
-        //
+        $middleware->statefulApi();
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
